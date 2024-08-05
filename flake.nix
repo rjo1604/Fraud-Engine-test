992ae1d041b014880efcfdff8692229a5d2e8030
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-08-05 20:44:19 +0530

    feat: webui mockup with tailwind

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "devshell for github:rjo1604/fraud-engine-test";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs;
+            [
+              vscode-langservers-extracted
+              (python311.withPackages(ps: with ps; [
+                flask
+                # add more python stuff here
+              ]))
+            ];
+          };
+        }
+      );
+}
